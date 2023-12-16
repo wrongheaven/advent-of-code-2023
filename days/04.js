3a33b58f4dbc48b4ed00b3cb3d473847d1456631
Henrik Engebretsen <henrik@wrongheaven.com>
2023-12-16 18:25:30 +0100

    Day 4 Part 1

---
+++ days/04.js
@@ -0,0 +1,70 @@
+import { getInput } from './utils.js';
+
+const inputArray = getInput('04');
+
+class Card
+{
+    id;
+    winningNumbers;
+    scratchedNumbers;
+    
+    winValue = 0;
+
+    constructor(cardInput)
+    {
+        // Split the card input into the card label and the card data
+        const [cardLabel, cardData] = cardInput.split(':');
+        this.id = parseInt(cardLabel.split(' ')[1]);
+
+        // Split the card data into the winning numbers and the scratched numbers
+        const [winningNumbers, scratchedNumbers] = cardData.trim().split(' | ').map((numbers) => {
+            // Parse the numbers into an array of integers
+            return numbers.split(' ').map((number) => {
+                return parseInt(number) || 0;
+            }).filter((number) => {
+                return number !== 0;
+            });
+        });
+
+        this.winningNumbers = winningNumbers;
+        this.scratchedNumbers = scratchedNumbers;
+    }
+
+    calcWinValue()
+    {
+        // How many of the scratched numbers are in the winning numbers?
+        const winNumCount = this.scratchedNumbers.filter((number) => {
+            return this.winningNumbers.includes(number);
+        }).length;
+
+        // Calculate the win value
+        this.winValue = winNumCount > 0
+            ? Math.pow(2, winNumCount - 1)
+            : 0
+        ;
+    }
+}
+
+part1();
+function part1()
+{
+    // Create an array of Card objects
+    const cards = inputArray.map((line) => {
+        const card = new Card(line);
+        // Calculate the win value for each card
+        card.calcWinValue();
+        return card;
+    });
+
+    const sumOfWinValues = cards.reduce((sum, card) => {
+        return sum + card.winValue;
+    }, 0);
+
+    console.log(`Day 4 Part 1: ${sumOfWinValues}`);
+}
+
+part2();
+function part2()
+{
+    console.log(`Day 4 Part 2: ${null}`);
+}
